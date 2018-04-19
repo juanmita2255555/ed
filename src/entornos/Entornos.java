bc401f569244b14ae8348d28d82c58e25d869e48
juanma <juanma@DESKTOP-E8KVF81>
2018-04-19 09:17:22 +0200

    Version inicial

---
+++ src/entornos/Entornos.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.io.Serializable;
+
+public class Entornos{
+    
+    private String nombre;
+    private int puntuacion;
+    private int prueba;
+    
+public Entornos(String nombre, int puntuacion){
+    
+    
+}
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+    
+    
+}
